62169c1b9eb7f4cb1988389c81428cde9cc30645
William Tessari <william.tessari@epitech.eu>
2024-03-07 23:36:47 +0100

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {} from "./components";
+
 const App = () => {
-  return <div>APPP</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">test</div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
